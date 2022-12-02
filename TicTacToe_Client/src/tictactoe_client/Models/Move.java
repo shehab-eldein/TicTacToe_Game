37b85f638c92d29f42e586a8925359606b1578e3
Hamed-mohamed-Git <hamed.arfat@icloud.com>
2022-12-02 23:00:40 +0200

    add-file Move

--- TicTacToe_Client/src/tictactoe_client/Models/Move.java
+++ TicTacToe_Client/src/tictactoe_client/Models/Move.java
@@ -4,12 +4,54 @@
  * and open the template in the editor.
  */
 package tictactoe_client.Models;
+import tictactoe_client.Enums.Shape;
 
 /**
  *
  * @author hamed
  */
 public class Move {
+    private Shape shape;
+    private String blockId;
+    
+    public Move(String id, Shape shape) {
+        blockId = id;
+        this.shape = shape;
+    }
+    
+    
+    private int getPostionByBlockId(){
+        switch (blockId) {
+            case "block1" :
+                return 0;
+            case "block2" :
+                return 1;
+            case "block3" :
+                return 2;
+            case "block4" :
+                return 3;
+            case "block5" :
+                return 4;
+            case "block6" :
+                return 5;
+            case "block7" :
+               return 6;
+            case "block8" :
+                return 7;
+            case "block9" :
+               return 8;
+        }
+        return -1;
+    }
+    
+    
+    public int getIndex(){
+        return getPostionByBlockId();
+    }
+    
+    public Shape getShape(){
+        return shape;
+    }
     
   
 }
