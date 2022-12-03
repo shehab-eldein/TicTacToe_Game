336147fed3239e2ce196a381ccb8299ed8907c84
shehab Eldein Ahmed <shehabahmed10001@gmail.com>
2022-12-04 00:13:02 +0200

    Add Game Class

---
+++ TicTacToe_Client/src/tictactoe_client/Models/Game.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe_client.Models;
+
+import java.util.ArrayList;
+import tictactoe_client.Enums.GameState;
+
+/**
+ *
+ * @author DELL
+ */
+public class Game {
+
+    Player playerX;
+    Player playerO;
+    Board board;
+    GameState gameState;
+    ArrayList<Move> history;
+    
+    public Game(Player playerX, Player playerO) {
+        this.playerX = playerX;
+        this.playerO = playerO;
+        board = new Board();
+        gameState = GameState.X_TURN;
+
+    }
+
+    GameState gameAction(Move move) {
+
+        if (board.isValidMove(move)) {
+            gameState = board.applyMove(move);
+            // add move to history 
+            history.add(move);
+        } else {
+            //not Valid Move
+        }
+        return gameState;
+    }
+    void save() {
+        //in data base in server
+        //OR
+        //in text File
+    }
+    
+
+}
