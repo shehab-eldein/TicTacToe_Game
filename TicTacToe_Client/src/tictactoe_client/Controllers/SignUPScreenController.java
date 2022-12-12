305b159bf81f57cc53c28113f61d110477fda350
shehab Eldein Ahmed <shehabahmed10001@gmail.com>
2022-12-12 20:37:02 +0200

    Create sign up controller

---
+++ TicTacToe_Client/src/tictactoe_client/Controllers/SignUPScreenController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe_client.Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author DELL
+ */
+public class SignUPScreenController implements Initializable {
+
+    @FXML
+    private TextField signUpUserNameTextField;
+    @FXML
+    private TextField signUpPasswordTextField;
+    @FXML
+    private Button signUpButton;
+    @FXML
+    private Label logInAccoutnButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void LogInButtonClick(ActionEvent event) {
+    }
+    
+}
