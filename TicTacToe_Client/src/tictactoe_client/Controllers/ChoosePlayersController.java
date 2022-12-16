4b2e3e3db3d3004fe50a4ddc29637ea17bbe6556
Rafeef <rafeefalmahy@gmail.com>
2022-12-16 16:37:24 +0200

    Handle getUsers response from server and add them to list in ChoosePlayerController

--- TicTacToe_Client/src/tictactoe_client/Controllers/ChoosePlayersController.java
+++ TicTacToe_Client/src/tictactoe_client/Controllers/ChoosePlayersController.java
@@ -6,7 +6,13 @@
 package tictactoe_client.Controllers;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -23,11 +29,14 @@ public class ChoosePlayersController implements Initializable {
     @FXML
     private Label backButton;
     @FXML
-    private ListView<?> onlinePlayersListView;
+    private ListView<String> onlinePlayersListView;
     @FXML
     private Button askUserButton;
     @FXML
     private Button playButton;
+    private GameHandler gameHandler;
+    private static List<String> clients = new ArrayList<>();
+    String usersName;
 
     /**
      * Initializes the controller class.
@@ -35,6 +44,33 @@ public class ChoosePlayersController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        //        try {
+        // TODO
+        /*  gameHandler.writeData("2");
+            gameHandler = GameHandler.getInstance((message) -> {
+            }, (response) -> {
+                usersName = response;
+            });
+            clients = splitRequest(usersName); */
+        clients = splitRequest("Rafeef-Esam-Ali-Almahy");
+        ObservableList<String> names = FXCollections.observableArrayList(
+                clients);
+        onlinePlayersListView.setItems(names);
+//        } catch (IOException ex) {
+//            Logger.getLogger(ChoosePlayersController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+
+    }
+
+    private List splitRequest(String data) {
+        List<String> queryList = Arrays.stream(data.split("\\-")) // split on comma
+                .map(str -> str.trim()) // remove white-spaces
+                .collect(Collectors.toList()); // collect to List  
+        return queryList;
+    }
+
+    private void ask() {
+        System.out.println(onlinePlayersListView.getSelectionModel().getSelectedItem());
+    }
+
 }
